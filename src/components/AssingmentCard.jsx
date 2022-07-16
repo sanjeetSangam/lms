1eeb2c3a35470e68baa64e4f63261ca36b5fddb7
sanjeetSangam <factsbe0@gmail.com>
2022-07-16 19:42:31 +0530

    dynamic assignment status added

--- src/components/AssingmentCard.jsx
+++ src/components/AssingmentCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { compareDate } from "../logic/date";
 
 export const AssingmentCard = ({
   title,
@@ -8,36 +9,58 @@ export const AssingmentCard = ({
   assignmentType,
   problemType,
 }) => {
+  const [assignmentStatus, setAssignmentStatus] = useState(undefined);
+
+  useEffect(() => {
+    if (compareDate(date) === true) {
+      setAssignmentStatus("COMPLETED");
+      return;
+    }
+
+    setAssignmentStatus(compareDate(date));
+  }, []);
+
   return (
     <div className="assingmentCard">
-
       <div className="__info">
-        
+        <div className="assignment__info">
+          <h3>{title}</h3>
+          <button
+            style={{
+              background:
+                assignmentType === "OJ"
+                  ? "red"
+                  : assignmentType === "FULL STACK"
+                  ? "purple"
+                  : assignmentType === "BACKEND"
+                  ? "#032727"
+                  : "#16a34a",
+            }}
+          >
+            {assignmentType}
+          </button>
+        </div>
+
+        <small>
+          <strong>{author}</strong> Scheduled <strong>{problemType}</strong> at{" "}
+          {date} - {time}
+        </small>
       </div>
 
-
-      <div className="assignment__info">
-        <h3>{title}</h3>
+      <div className="__status">
         <button
           style={{
             background:
-              assignmentType === "OJ"
-                ? "red"
-                : assignmentType === "FULL STACK"
-                ? "purple"
-                : assignmentType === "BACKEND"
-                ? "#032727"
-                : "#16a34a",
+              assignmentStatus === "UPCOMING"
+                ? "gray"
+                : assignmentStatus === "LIVE"
+                ? "GREEN"
+                : "#024cac",
           }}
         >
-          {assignmentType}
+          {assignmentStatus}
         </button>
       </div>
-
-      <small>
-        <strong>{author}</strong> Scheduled <strong>{problemType}</strong> at{" "}
-        {date} - {time}
-      </small>
     </div>
   );
 };
