18548624e1115183751ae002aff299bf78aefb88
sanjeetSangam <factsbe0@gmail.com>
2022-07-16 19:51:11 +0530

    daily date added in home page

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -1,22 +1,26 @@
 import React from "react";
+import { IoIosBriefcase } from "react-icons/io";
 
 export const HomeCard = () => {
   return (
     <div className="homeCard">
       <div className="daily__info">
         <div className="card__time">
-          <p>09:00 AM</p>
+          <p>
+            {" "}
+            <IoIosBriefcase /> 09:00 AM
+          </p>
           <p>LECTURE</p>
         </div>
         <div className="card__info">
-          <h2>Scrum 15.07.2022</h2>
+          <h2>Scrum {new Date().toISOString().slice(0, 10)}</h2>
           <p>
             <strong>Sibin O</strong> Starting at <strong>09:00 AM</strong>
           </p>
 
           <div className="btns">
-            <button className="scrm" >SCRUM</button>
-            <button className="live" >LIVE</button>
+            <button className="scrm">SCRUM</button>
+            <button className="live">LIVE</button>
           </div>
         </div>
       </div>
